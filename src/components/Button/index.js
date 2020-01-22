25043b52f5eee5230c833cf193d88153a42d8084
soupette <cyril.lpz@gmail.com>
2020-01-22 12:18:15 +0100

    Add compo folder
    Signed-off-by: soupette <cyril.lpz@gmail.com>

---
+++ src/components/Button/index.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * Button
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function Button() {
+  return (
+    <div>
+      Button
+    </div>
+  );
+}
+
+Button.defaultProps = {};
+Button.propTypes = {};
+
+export default Button;
